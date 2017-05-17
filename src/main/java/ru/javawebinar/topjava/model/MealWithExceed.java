ae05f1a26b699fd5668a52ce7bb750b3b3668fa3
HasanKarilov <hkarilov@gmail.com>
2017-05-17 21:29:20 +0600

    HW0 stream

---
+++ src/main/java/ru/javawebinar/topjava/model/MealWithExceed.java
@@ -0,0 +1,33 @@
+package ru.javawebinar.topjava.model;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by hanaria on 5/17/17.
+ */
+public class MealWithExceed {
+    private final LocalDateTime dateTime;
+
+    private final String description;
+
+    private final int calories;
+
+    private final boolean exceed;
+
+    public MealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed) {
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+        this.exceed = exceed;
+    }
+
+    @Override
+    public String toString() {
+        return "UserMealWithExceed{" +
+                "dateTime=" + dateTime +
+                ", description='" + description + '\'' +
+                ", calories=" + calories +
+                ", exceed=" + exceed +
+                '}';
+    }
+}
