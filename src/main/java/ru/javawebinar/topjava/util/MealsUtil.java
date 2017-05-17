0afe6e0bf247829911fbb6fcb682e0c0b5c51fa2
HasanKarilov <hkarilov@gmail.com>
2017-05-18 01:54:41 +0600

    HW0 add some comments

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -23,21 +23,33 @@ public class MealsUtil {
                 new Meal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
                 new Meal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
         );
+        // Не превышает ли норму в 2000 каллорий в день. И фильтрируем за периуд с 07:00 - 12:00
         List<MealWithExceed> mealsWithExceeded = getFilteredWithExceeded(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+
         mealsWithExceeded.forEach(System.out::println);
 
         System.out.println(getFilteredWithExceededByCycle(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
     public static List<MealWithExceed> getFilteredWithExceeded(List<Meal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        //collect - Представление результатов в виде коллекций и других структур данных
         Map<LocalDate, Integer> caloriesSumByDate = meals.stream()
                 .collect(
                         Collectors.groupingBy(Meal::getDate, Collectors.summingInt(Meal::getCalories))
 //                      Collectors.toMap(Meal::getDate, Meal::getCalories, Integer::sum)
                 );
+        for(Map.Entry<LocalDate, Integer> map: caloriesSumByDate.entrySet()){
+            System.err.println(map.getKey() + " - " + map.getValue());
+            /*
+            2015-05-30 - 2000
+            2015-05-31 - 2010
+             */
+        }
 
         return meals.stream()
+                // filter - Отфильтровывает записи, возвращает только записи, соответствующие условию
                 .filter(meal -> TimeUtil.isBetween(meal.getTime(), startTime, endTime))
+                // map - Преобразует каждый элемент стрима
                 .map(meal -> createWithExceed(meal, caloriesSumByDate.get(meal.getDate()) > caloriesPerDay))
                 .collect(Collectors.toList());
     }
